e3158819f478625b742908bfa15835c951d663aa
Andrew Finberg <afinberg2464@students.mc3.edu>
2019-10-05 13:45:53 -0400

    squash! Fixed Guitar class

--- src/com/andrewfinberg/ElectricGtr.java
+++ src/com/andrewfinberg/ElectricGtr.java
@@ -11,10 +11,17 @@ public class ElectricGtr extends Guitar {
     /**
      * Default constructor
      *
+     * @param make
+     * @param model
+     * @param bodyStyle
+     * @param bodyWood
+     * @param color
+     * @param frets
      * @param pickupType
      * @param bridgeType
      */
-    public ElectricGtr(String pickupType, String bridgeType) {
+    public ElectricGtr(String make, String model, String bodyStyle, String bodyWood, String color, int frets, String pickupType, String bridgeType) {
+        super(make, model, bodyStyle, bodyWood, color, frets);
         this.pickupType = pickupType;
         this.bridgeType = bridgeType;
     }
